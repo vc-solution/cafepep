f842fecb42604aced1d9e7bb1386866420cff509
Peter <macherey@vc-solution.de>
2013-09-23 21:08:15 +0200

    Use right url for activation mail!

--- src/main/java/localdomain/localhost/RegistrationService.java
+++ src/main/java/localdomain/localhost/RegistrationService.java
@@ -3,8 +3,10 @@ package localdomain.localhost;
 import java.security.MessageDigest;
 
 import javax.ejb.Stateless;
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
@@ -31,6 +33,9 @@ public class RegistrationService {
 	
 	public void sendRegistrationMail(User user)
 	{
+		HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+		String url = req.getRequestURL().toString();
+		url=url.substring(0, url.lastIndexOf("/"));
 		Email email = new SimpleEmail();
 		email.setHostName("smtp.1und1.de");
 		email.setSmtpPort(465);
@@ -39,7 +44,7 @@ public class RegistrationService {
 		try {
 		email.setFrom("macherey@vc-solution.de");
 		email.setSubject("CafePEP Account Verification ");
-		email.setMsg("http://localhost:8080/glassfish4-clickstart/activation.xhtml?key="+user.getVerification_key());
+		email.setMsg(url+"/activation.xhtml?key="+user.getVerification_key());
 		email.addTo(user.getEmail());
 		email.send();
 		} catch (EmailException e) {
